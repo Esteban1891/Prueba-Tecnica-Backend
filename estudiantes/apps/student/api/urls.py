1dee062876533ad1cdd6ada702059a1666729d63
Esteban1891 <esteban.delahoz15@gmail.com>
2021-06-02 04:23:59 -0500

    file what contains the urls to the apis

--- estudiantes/apps/student/api/urls.py
+++ estudiantes/apps/student/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path 
 from apps.student.api.api import admin_api_view, student_detail_api_view, admin_detail_api_view, admin_list_api_view
 
-
 urlpatterns = [
     path('students/', admin_api_view, name = 'student'),
     path('students/<pk>/', student_detail_api_view, name = 'student'),
